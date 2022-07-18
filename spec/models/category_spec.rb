d51ee6ec80c34e0da8a3b80fe0207d8a782d814d
Magno Felipe <mfts201116@gmail.com>
2022-07-18 11:44:23 -0300

    Grouping the test in models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  # Teste de associação com os livros
-  it { should have_many(:books) }
-  # Teste de verificação da presença do atributo name
-  it { should validate_presence_of(:name) }
-  # Teste de verificação do tamanho do atributo name
-  it { should validate_length_of(:name).is_at_least(3) }
+  describe "associations" do
+    # Teste de associação com os livros
+    it { should have_many(:books) }
+  end
+
+  describe "validations" do
+    # Teste de verificação da presença do atributo name
+    it { should validate_presence_of(:name) }
+    # Teste de verificação do tamanho do atributo name
+    it { should validate_length_of(:name).is_at_least(3) }
+  end
 end
