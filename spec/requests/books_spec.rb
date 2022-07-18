bd0dbabec2f0eaa7f9f8975c176816ba7cdc0246
Magno Felipe <mfts201116@gmail.com>
2022-07-18 13:26:50 -0300

    Creating the test to category controller

---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Books", type: :request do
+  # inicializando os dados
+  # let!(:books) { create_list(:book, 5) }
+  # let!(:book_id) { books.first.id }
+
+  # describe "GET /books" do
+  #   before { get "/api/v1/books" }
+  #   it "returns books" do
+  #     expect(json).not_to be_empty
+  #     expect(json.size).to eq(5)
+  #   end
+  #   it "returns status code 200" do
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+end
