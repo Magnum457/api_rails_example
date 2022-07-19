12a70d2df88173303610d3e291be0b6595efc67d
Magno Felipe <mfts201116@gmail.com>
2022-07-19 08:30:04 -0300

    Fixing the book representer

--- app/controllers/representers/books_representer.rb
+++ app/controllers/representers/books_representer.rb
@@ -9,7 +9,7 @@ class BooksRepresenter
                 id: book.id,
                 title: book.title,
                 author: book.author,
-                category: Category.find(book.id).name,
+                category: Category.find(book.category_id).name,
                 data_added: book.created_at
             }
         end
